29d688e0c183d59cce0be0d4e19d45014e4dff21
demosocket <demosocket@gmail.com>
2020-05-27 12:10:51 +0300

    added db-properties

---
+++ src/main/java/com/demosocket/blog/config/PersistenceConfig.java
@@ -0,0 +1,37 @@
+package com.demosocket.blog.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.util.Objects;
+
+@Configuration
+@ComponentScan("com.demosocket.blog")
+@PropertySource("classpath:application.properties")
+public class PersistenceConfig {
+
+    private final Environment environment;
+
+    @Autowired
+    public PersistenceConfig(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(Objects.requireNonNull(
+                environment.getProperty("spring.datasource.driver-class-name")));
+        dataSource.setUrl(environment.getProperty("spring.datasource.url"));
+        dataSource.setUsername(environment.getProperty("spring.datasource.username"));
+        dataSource.setPassword(environment.getProperty("spring.datasource.password"));
+
+        return dataSource;
+    }
+}
